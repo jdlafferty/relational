0247fdab22f09ee65b89f5eb12477e4b23160bb2
John Lafferty <lafferty@gmail.com>
2023-03-16 10:24:22 -0500

    m

---
+++ experiments/set/set_abstractor_models.py
@@ -0,0 +1,109 @@
+import tensorflow as tf
+from tensorflow.keras import layers
+from seq2seq_transformer import Encoder, AddPositionalEmbedding
+from contextual_decoder import ContextualCrossAttention, ContextDecoderLayer, ContextDecoder
+from abstracters import RelationalAbstracter
+
+
+class SetTransformer(tf.keras.Model):
+    def __init__(self, num_layers, num_heads, dff,
+            input_vocab_size, target_vocab_size, embedding_dim,
+            dropout_rate=0.1, name='set_transformer'):
+        super().__init__(name=name)
+
+        self.source_embedder = layers.TimeDistributed(layers.Dense(embedding_dim), name='source_embedder')
+        self.target_embedder = layers.Embedding(target_vocab_size, embedding_dim, name='target_embedder')
+
+        self.pos_embedding_adder_input = AddPositionalEmbedding(name='add_pos_embedding_input')
+        self.pos_embedding_adder_target = AddPositionalEmbedding(name='add_pos_embedding_target')
+
+        self.encoder = Encoder(num_layers=num_layers, num_heads=num_heads, dff=dff, dropout_rate=dropout_rate, name='encoder')
+        self.decoder = ContextDecoder(num_layers=num_layers, num_heads=num_heads, dff=dff,
+          dropout_rate=dropout_rate, name='decoder')
+
+        self.final_layer = layers.Dense(target_vocab_size, name='final_layer')
+
+
+    def call(self, inputs):
+        source, target  = inputs
+
+        x = self.source_embedder(source)
+        x = self.pos_embedding_adder_input(x)
+
+        encoder_context = self.encoder(x)
+
+        target_embedding = self.target_embedder(target)
+        target_embedding = self.pos_embedding_adder_target(target_embedding)
+
+        x = self.decoder(input_seq=target_embedding, query_seq=target_embedding, key_seq=encoder_context, value_seq=encoder_context)
+
+        # Final linear layer output.
+        logits = self.final_layer(x)  # (batch_size, target_len, target_vocab_size)
+
+        #logits = tf.linalg.matmul(encoder_context, tf.transpose(x, perm=(0,2,1)))
+        #logits = tf.transpose(logits, perm=(0,2,1))
+
+        try:
+          # Drop the keras mask, so it doesn't scale the losses/metrics.
+          # b/250038731
+          del logits._keras_mask
+        except AttributeError:
+          pass
+
+        # Return the final output and the attention weights.
+        return logits
+
+
+class SetAbstractor(tf.keras.Model):
+    def __init__(self, num_layers, num_heads, dff,
+            input_vocab_size, target_vocab_size, embedding_dim,
+            dropout_rate=0.1, name='set_seq2seq_abstracter'):
+        super().__init__(name=name)
+
+        self.source_embedder = layers.TimeDistributed(layers.Dense(embedding_dim), name='source_embedder')
+        self.target_embedder = layers.Embedding(target_vocab_size, embedding_dim, name='target_embedder')
+
+
+        self.pos_embedding_adder_input = AddPositionalEmbedding(name='add_pos_embedding_input')
+        self.pos_embedding_adder_target = AddPositionalEmbedding(name='add_pos_embedding_target')
+
+        self.encoder = Encoder(num_layers=num_layers, num_heads=num_heads, dff=dff, dropout_rate=dropout_rate, name='encoder')
+        self.abstracter = RelationalAbstracter(num_layers=num_layers, num_heads=num_heads, dff=dff, dropout_rate=dropout_rate, name='abstracter')
+        self.decoder = ContextDecoder(num_layers=num_layers, num_heads=num_heads, dff=dff,
+          dropout_rate=dropout_rate, name='decoder')
+
+        self.final_layer = layers.Dense(target_vocab_size, name='final_layer')
+
+
+    def call(self, inputs):
+        # To use a Keras model with `.fit` you must pass all your inputs in the
+        # first argument.
+        source, target  = inputs
+
+        x = self.source_embedder(source)
+        x = self.pos_embedding_adder_input(x)
+
+        encoder_context = self.encoder(x)
+
+        abstracted_context = self.abstracter(encoder_context)
+
+        target_embedding = self.target_embedder(target)
+        target_embedding = self.pos_embedding_adder_target(target_embedding)
+
+        x = self.decoder(input_seq=target_embedding, query_seq=abstracted_context, key_seq=abstracted_context, value_seq=encoder_context)
+
+        # Final linear layer output.
+        logits = self.final_layer(x)  # (batch_size, target_len, target_vocab_size)
+
+        #logits = tf.linalg.matmul(encoder_context, tf.transpose(x, perm=(0,2,1)))
+        #logits = tf.transpose(logits, perm=(0,2,1))
+
+        try:
+          # Drop the keras mask, so it doesn't scale the losses/metrics.
+          # b/250038731
+          del logits._keras_mask
+        except AttributeError:
+          pass
+
+        # Return the final output and the attention weights.
+        return logits
