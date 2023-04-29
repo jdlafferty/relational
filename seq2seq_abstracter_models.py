7b14b8eadccdb7cb8d64a7400be5a678124d90bc
Awni Altabaa <awnyaltabaa@gmail.com>
2023-04-28 20:03:51 -0400

    updated seq2seq_abstracter_models updated to support specifying rel_attention_activation

--- seq2seq_abstracter_models.py
+++ seq2seq_abstracter_models.py
@@ -82,7 +82,7 @@ class Transformer(tf.keras.Model):
 
 
 class Seq2SeqRelationalAbstracter(tf.keras.Model):
-    def __init__(self, num_layers, num_heads, dff,
+    def __init__(self, num_layers, num_heads, dff, rel_attention_activation,
             input_vocab, target_vocab, embedding_dim, output_dim,
             dropout_rate=0.1, name='seq2seq_relational_abstracter'):
         """
@@ -91,7 +91,8 @@ class Seq2SeqRelationalAbstracter(tf.keras.Model):
         Args:
             num_layers (int): # of layers in encoder and decoder
             num_heads (int): # of attention heads in attention operations
-            dff (int): dimension of feedforward laeyrs
+            dff (int): dimension of feedforward layers
+            rel_attention_activation (str): the activation function to use in relational attention.
             input_vocab (int or str): if input is tokens, the size of vocabulary as an int. 
                 if input is vectors, the string 'vector'. used to create embedder.
             target_vocab (int): if target is tokens, the size of the vocabulary as an int. 
@@ -126,7 +127,8 @@ class Seq2SeqRelationalAbstracter(tf.keras.Model):
         self.pos_embedding_adder_target = AddPositionalEmbedding(name='add_pos_embedding_target')
 
         self.encoder = Encoder(num_layers=num_layers, num_heads=num_heads, dff=dff, dropout_rate=dropout_rate, name='encoder')
-        self.abstracter = RelationalAbstracter(num_layers=num_layers, num_heads=num_heads, dff=dff, dropout_rate=dropout_rate, name='abstracter')
+        self.abstracter = RelationalAbstracter(num_layers=num_layers, num_heads=num_heads, dff=dff,
+            mha_activation_type=rel_attention_activation, dropout_rate=dropout_rate, name='abstracter')
         self.decoder = Decoder(num_layers=num_layers, num_heads=num_heads, dff=dff,
           dropout_rate=dropout_rate, name='decoder')
         self.final_layer = layers.Dense(output_dim, name='final_layer')
