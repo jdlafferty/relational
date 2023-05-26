e0b857863241638803f61715c167b058709d14dc
Awni Altabaa <awnyaltabaa@gmail.com>
2023-05-26 14:00:19 -0400

    updated evaluate_argsort_model_learning_curves.py

--- experiments/object_argsort_autoregressive/evaluate_argsort_model_learning_curves.py
+++ experiments/object_argsort_autoregressive/evaluate_argsort_model_learning_curves.py
@@ -167,50 +167,6 @@ autoreg_abstractor_kwargs = dict(
 
 # endregion
 
-# # region evaluation code
-# def evaluate_seq2seq_model(model, source_test, target_test, labels_test, start_token, print_=False):
-    
-#     n = len(source_test)
-#     output = np.zeros(shape=(n, (seqs_length+1)), dtype=int)
-#     output[:,0] = start_token
-#     for i in range(seqs_length):
-#         predictions = model((source_test, output[:, :-1]), training=False)
-#         predictions = predictions[:, i, :]
-#         predicted_id = tf.argmax(predictions, axis=-1)
-#         output[:,i+1] = predicted_id
-
-#     elementwise_acc = (np.mean(output[:,1:] == labels_test))
-#     acc_per_position = [np.mean(output[:, i+1] == labels_test[:, i]) for i in range(seqs_length)]
-#     seq_acc = np.mean(np.all(output[:,1:]==labels_test, axis=1))
-
-
-#     teacher_forcing_acc = teacher_forcing_acc_metric(labels_test, model([source_test, target_test]))
-#     teacher_forcing_acc_metric.reset_state()
-
-#     if print_:
-#         print('element-wise accuracy: %.2f%%' % (100*elementwise_acc))
-#         print('full sequence accuracy: %.2f%%' % (100*seq_acc))
-#         print('teacher-forcing accuracy:  %.2f%%' % (100*teacher_forcing_acc))
-
-
-#     return_dict = {
-#         'elementwise_accuracy': elementwise_acc, 'full_sequence_accuracy': seq_acc,
-#         'teacher_forcing_accuracy': teacher_forcing_acc, 'acc_by_position': acc_per_position
-#         }
-
-#     return return_dict
-
-# def log_to_wandb(model, evaluation_dict):
-#     acc_by_position_table = wandb.Table(
-#         data=[(i, acc) for i, acc in enumerate(evaluation_dict['acc_by_position'])], 
-#         columns=["position", "element-wise accuracy at position"])
-
-#     evaluation_dict['acc_by_position'] = wandb.plot.line(
-#         acc_by_position_table, "position", "element-wise accuracy at position",
-#         title="Element-wise Accuracy By Position")
-
-#     wandb.log(evaluation_dict)
-
 max_train_size = args.max_train_size
 train_size_step = args.train_size_step
 min_train_size = args.min_train_size
