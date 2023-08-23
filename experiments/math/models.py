c53ec713df0d0b2369d1a0a8e6002a55e62eb7a7
Awni <awnyaltabaa@gmail.com>
2023-08-22 23:39:30 -0400

    updated models.py

--- experiments/math/models.py
+++ experiments/math/models.py
@@ -88,59 +88,6 @@ def create_abstractor2(input_vocab_size, target_vocab_size, size='x-large'):
     return abstractor
 #endregion
 
-#region Abstractor3
-def create_abstractor3(input_vocab_size, target_vocab_size, size='x-large'):
-    d_model, num_heads, dff, num_layers = get_params_by_size(size)
-
-    encoder_kwargs = dict(num_layers=num_layers, num_heads=num_heads, dff=dff, dropout_rate=0.1,)
-    decoder_kwargs = dict(num_layers=num_layers, num_heads=num_heads, dff=dff, dropout_rate=0.1,)
-    abstractor_kwargs = dict(num_layers=num_layers, rel_dim=num_heads, symbol_dim=d_model,
-        proj_dim=d_model//num_heads, symmetric_rels=False, encoder_kwargs=None,
-        rel_activation_type='tanh', use_self_attn=False, use_layer_norm=False,
-        dropout_rate=0.1)
-
-    abstractor = AutoregressiveAbstractor(
-        encoder_kwargs,
-        abstractor_kwargs,
-        decoder_kwargs,
-        input_vocab=input_vocab_size,
-        target_vocab=target_vocab_size,
-        embedding_dim=d_model,
-        output_dim=target_vocab_size,
-        abstractor_type='abstractor', # 'abstractor', 'simple', 'relational', or 'symbolic'
-        abstractor_on='input', # 'input' or 'encoder'
-        decoder_on='encoder-abstractor', # 'abstractor' or 'encoder-abstractor'
-        name='autoregressive_abstractor')
-    return abstractor
-#endregion
-
-#region Abstractor4
-def create_abstractor4(input_vocab_size, target_vocab_size, size='x-large'):
-    d_model, num_heads, dff, num_layers = get_params_by_size(size)
-
-    encoder_kwargs = dict(num_layers=num_layers, num_heads=num_heads, dff=dff, dropout_rate=0.1,)
-    decoder_kwargs = dict(num_layers=num_layers, num_heads=num_heads, dff=dff, dropout_rate=0.1,)
-    abstractor_kwargs = dict(num_layers=num_layers, rel_dim=num_heads, symbol_dim=d_model,
-        proj_dim=d_model//num_heads, symmetric_rels=False, encoder_kwargs=None,
-        rel_activation_type='softmax', use_self_attn=True, use_layer_norm=True,
-        dropout_rate=0.1)
-
-    abstractor = AutoregressiveAbstractor(
-        encoder_kwargs,
-        abstractor_kwargs,
-        decoder_kwargs,
-        input_vocab=input_vocab_size,
-        target_vocab=target_vocab_size,
-        embedding_dim=d_model,
-        output_dim=target_vocab_size,
-        abstractor_type='abstractor', # 'abstractor', 'simple', 'relational', or 'symbolic'
-        abstractor_on='encoder', # 'input' or 'encoder'
-        decoder_on='encoder-abstractor', # 'abstractor' or 'encoder-abstractor'
-        name='autoregressive_abstractor')
-    return abstractor
-#endregion
-
-
 #region RelationalAbstractor
 def create_relational_abstractor(input_vocab_size, target_vocab_size, size='x-large'):
     d_model, num_heads, dff, num_layers = get_params_by_size(size)
@@ -165,10 +112,36 @@ def create_relational_abstractor(input_vocab_size, target_vocab_size, size='x-la
     return abstractor
 #endregion
 
+#region RelationalAbstractor
+def create_relational_abstractor2(input_vocab_size, target_vocab_size, size='x-large'):
+    d_model, num_heads, dff, num_layers = get_params_by_size(size)
+
+    encoder_kwargs = dict(num_layers=num_layers, num_heads=num_heads, dff=dff, dropout_rate=0.1,)
+    decoder_kwargs = dict(num_layers=num_layers, num_heads=num_heads, dff=dff, dropout_rate=0.1,)
+    abstractor_kwargs = dict(num_layers=num_layers, num_heads=num_heads, dff=dff,
+        use_learned_symbols=False, mha_activation_type='softmax', use_self_attn=True)
+
+    abstractor = AutoregressiveAbstractor(
+        encoder_kwargs,
+        abstractor_kwargs,
+        decoder_kwargs,
+        input_vocab=input_vocab_size,
+        target_vocab=target_vocab_size,
+        embedding_dim=d_model,
+        output_dim=target_vocab_size,
+        abstractor_type='relational', # 'abstractor', 'simple', 'relational', or 'symbolic'
+        abstractor_on='input', # 'input' or 'encoder'
+        decoder_on='encoder-abstractor', # 'abstractor' or 'encoder-abstractor'
+        name='autoregressive_abstractor')
+    return abstractor
+#endregion
+
 model_creator_dict = dict(
     transformer=create_transformer,
     abstractor=create_abstractor,
     abstractor2=create_abstractor2,
     abstractor3=create_abstractor3,
     abstractor4=create_abstractor4,
-    relational_abstractor=create_relational_abstractor)
+    relational_abstractor=create_relational_abstractor,
+    relational_abstractor2=create_relational_abstractor2
+    )
