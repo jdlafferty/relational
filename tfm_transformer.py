5bfa9e6521884705fabcf19d2a4532fd82096932
Awni <awnyaltabaa@gmail.com>
2023-09-09 19:24:01 -0400

    added tfm_transformer.py

---
+++ tfm_transformer.py
@@ -0,0 +1,63 @@
+"""An implementation of a Seq2Seq Transformer using tensorflow_models"""
+
+import tensorflow_models as tfm
+from tensorflow.keras import layers
+from transformer_modules import AddPositionalEmbedding
+
+class TFMTransformer(tf.keras.Model):
+    def __init__(self, num_layers, num_heads, dff,
+            input_vocab, target_vocab, embedding_dim, output_dim,
+            dropout_rate=0.1, name='transformer'):
+
+        super().__init__(name=name)
+
+        if isinstance(input_vocab, int):
+            self.source_embedder = layers.Embedding(input_vocab, embedding_dim, name='source_embedder')
+        elif input_vocab == 'vector':
+            self.source_embedder = layers.TimeDistributed(layers.Dense(embedding_dim), name='source_embedder')
+        else:
+            raise ValueError(
+                "`input_vocab` must be an integer if the input sequence is token-valued or "
+                "'vector' if the input sequence is vector-valued.")
+
+        if isinstance(target_vocab, int):
+            self.target_embedder = layers.Embedding(target_vocab, embedding_dim, name='target_embedder')
+        elif target_vocab == 'vector':
+            self.target_embedder = layers.TimeDistributed(layers.Dense(embedding_dim), name='target_embedder')
+        else:
+            raise ValueError(
+                "`input_vocab` must be an integer if the input sequence is token-valued or "
+                "'vector' if the input sequence is vector-valued.")
+
+        self.pos_embedding_adder_input = AddPositionalEmbedding(name='add_pos_embedding_input')
+        self.pos_embedding_adder_target = AddPositionalEmbedding(name='add_pos_embedding_target')
+
+        self.encoder = tfm.nlp.models.TransformerEncoder(num_layers=num_layers, num_attention_heads=num_heads, intermediate_size=dff, dropout_rate=dropout_rate)
+        self.decoder = tfm.nlp.models.TransformerDecoder(num_layers=num_layers, num_attention_heads=num_heads, intermediate_size=dff, dropout_rate=dropout_rate)
+
+        self.final_layer = layers.Dense(output_dim, name='final_layer')
+
+
+    def call(self, inputs):
+        source, target  = inputs
+
+        x = self.source_embedder(source)
+        x = self.pos_embedding_adder_input(x)
+
+        encoder_context = self.encoder(x)
+
+        target_embedding = self.target_embedder(target)
+        target_embedding = self.pos_embedding_adder_target(target_embedding)
+
+        x = self.decoder(target_embedding, encoder_context)
+
+        logits = self.final_layer(x)
+
+        try:
+          # Drop the keras mask, so it doesn't scale the losses/metrics.
+          # b/250038731
+          del logits._keras_mask
+        except AttributeError:
+          pass
+
+        return logits
