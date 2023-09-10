843c396d42d5ca98175111f19d765883c8a1e197
Awni <awnyaltabaa@gmail.com>
2023-09-09 20:27:43 -0400

    updated added tfm models

--- experiments/math/models.py
+++ experiments/math/models.py
@@ -1,7 +1,8 @@
 import sys; sys.path += ['..', '../..']
 from autoregressive_abstractor import AutoregressiveAbstractor
 from seq2seq_abstracter_models import Transformer
-
+from tfm_transformer import TFMTransformer
+from tfm_abstractor import TFMAutoregressiveAbstractor
 
 #region common kwargs
 
@@ -33,6 +34,17 @@ def get_params_by_size(size):
 def create_transformer(input_vocab_size, target_vocab_size, size='x-large'):
     d_model, num_heads, dff, num_layers = get_params_by_size(size)
     transformer = Transformer(
+        num_layers=num_layers, num_heads=num_heads, dff=dff, embedding_dim=d_model,
+        input_vocab=input_vocab_size, target_vocab=target_vocab_size,
+        output_dim=target_vocab_size, dropout_rate=0.1,)
+
+    return transformer
+#endregion
+
+#region TFM-Transformer
+def create_tfmtransformer(input_vocab_size, target_vocab_size, size='x-large'):
+    d_model, num_heads, dff, num_layers = get_params_by_size(size)
+    transformer = TFMTransformer(
         num_layers=num_layers, num_heads=num_heads, dff=dff, embedding_dim=d_model,
         input_vocab=input_vocab_size, target_vocab=target_vocab_size,
         output_dim=target_vocab_size, dropout_rate=0.1,)
@@ -164,9 +176,66 @@ def create_relational_abstractor2(input_vocab_size, target_vocab_size, size='x-l
     return abstractor
 #endregion
 
+#region TFM-Abstractor
+def create_tfm_abstractor(input_vocab_size, target_vocab_size, size='x-large'):
+    d_model, num_heads, dff, num_layers = get_params_by_size(size)
+    encoder_kwargs = dict(num_layers=num_layers, num_attention_heads=num_heads, intermediate_size=dff, dropout_rate=0.1,)
+    decoder_kwargs = dict(num_layers=num_layers, num_heads=num_heads, dff=dff, dropout_rate=0.1,)
+    abstractor_kwargs = dict(
+        num_layers=num_layers, rel_dim=num_heads, dff=dff, symbol_dim=d_model,
+        proj_dim=d_model//num_heads, symmetric_rels=False, use_learned_symbols=False,
+        rel_activation_type='linear', use_self_attn=False, use_layer_norm=False,
+        dropout_rate=0.1)
+    # abstractor_kwargs = dict(num_layers=num_layers, num_heads=num_heads, dff=dff,
+    #     use_learned_symbols=False, mha_activation_type='linear', use_self_attn=False)
+
+
+    abstractor = TFMAutoregressiveAbstractor(
+        encoder_kwargs,
+        abstractor_kwargs,
+        decoder_kwargs,
+        input_vocab=input_vocab_size,
+        target_vocab=target_vocab_size,
+        embedding_dim=d_model,
+        output_dim=target_vocab_size,
+        abstractor_type='abstractor', # 'abstractor', 'simple', 'relational', or 'symbolic'
+        abstractor_on='encoder', # 'input' or 'encoder'
+        decoder_on='encoder-abstractor', # 'abstractor' or 'encoder-abstractor'
+        name='autoregressive_abstractor')
+
+    return abstractor
+#endregion
+
+#region TFM-Abstractor
+def create_tfm_relational_abstractor(input_vocab_size, target_vocab_size, size='x-large'):
+    d_model, num_heads, dff, num_layers = get_params_by_size(size)
+    encoder_kwargs = dict(num_layers=num_layers, num_attention_heads=num_heads, intermediate_size=dff, dropout_rate=0.1,)
+    decoder_kwargs = dict(num_layers=num_layers, num_heads=num_heads, dff=dff, dropout_rate=0.1,)
+    abstractor_kwargs = dict(num_layers=num_layers, num_heads=num_heads, dff=dff,
+        use_learned_symbols=False, mha_activation_type='softmax', use_self_attn=False)
+
+    abstractor = TFMAutoregressiveAbstractor(
+        encoder_kwargs,
+        abstractor_kwargs,
+        decoder_kwargs,
+        input_vocab=input_vocab_size,
+        target_vocab=target_vocab_size,
+        embedding_dim=d_model,
+        output_dim=target_vocab_size,
+        abstractor_type='relational', # 'abstractor', 'simple', 'relational', or 'symbolic'
+        abstractor_on='encoder', # 'input' or 'encoder'
+        decoder_on='encoder-abstractor', # 'abstractor' or 'encoder-abstractor'
+        name='autoregressive_abstractor')
+
+    return abstractor
+#endregion
+
 model_creator_dict = dict(
     transformer=create_transformer,
+    tfm_transformer=create_tfmtransformer,
     abstractor=create_abstractor,
+    tfm_abstractor=create_tfm_abstractor,
+    tfm_relational_abstractor=create_tfm_relational_abstractor,
     abstractor2=create_abstractor2,
     relational_abstractor=create_relational_abstractor,
     linear_relational_abstractor=create_linear_relational_abstractor,
