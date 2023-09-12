e656863a5cc865980d7bd870a73f131068ef6ffa
Awni <awnyaltabaa@gmail.com>
2023-09-11 22:31:10 -0400

    updated

--- experiments/math/models.py
+++ experiments/math/models.py
@@ -259,6 +259,30 @@ def create_tfm_relational_abstractor(input_vocab_size, target_vocab_size, size='
 #endregion
 
 #region TFM-Abstractor
+def create_tfm_1layer_relational_abstractor(input_vocab_size, target_vocab_size, size='x-large'):
+    d_model, num_heads, dff, num_layers = get_params_by_size(size)
+    encoder_kwargs = dict(num_layers=num_layers, num_attention_heads=num_heads, intermediate_size=dff, dropout_rate=0.1,)
+    decoder_kwargs = dict(num_layers=num_layers, num_heads=num_heads, dff=dff, dropout_rate=0.1,)
+    abstractor_kwargs = dict(num_layers=1, num_heads=num_heads, dff=dff,
+        use_learned_symbols=False, mha_activation_type='softmax', use_self_attn=False)
+
+    abstractor = TFMAutoregressiveAbstractor(
+        encoder_kwargs,
+        abstractor_kwargs,
+        decoder_kwargs,
+        input_vocab=input_vocab_size,
+        target_vocab=target_vocab_size,
+        embedding_dim=d_model,
+        output_dim=target_vocab_size,
+        abstractor_type='relational', # 'abstractor', 'simple', 'relational', or 'symbolic'
+        abstractor_on='encoder', # 'input' or 'encoder'
+        decoder_on='encoder-abstractor', # 'abstractor' or 'encoder-abstractor'
+        name='autoregressive_abstractor')
+
+    return abstractor
+#endregion
+
+#region TFM-Abstractor
 def create_tfm_relational_abstractor2(input_vocab_size, target_vocab_size, size='x-large'):
     d_model, num_heads, dff, num_layers = get_params_by_size(size)
     encoder_kwargs = dict(num_layers=num_layers, num_attention_heads=num_heads, intermediate_size=dff, dropout_rate=0.1,)
@@ -312,6 +336,7 @@ model_creator_dict = dict(
     abstractor=create_abstractor,
     tfm_abstractor=create_tfm_abstractor,
     tfm_relational_abstractor=create_tfm_relational_abstractor,
+    tfm_1layer_relational_abstractor=create_tfm_1layer_relational_abstractor,
     tfm_relational_abstractor2=create_tfm_relational_abstractor2,
     tfm_linear_relational_abstractor=create_tfm_relational_abstractor,
     tfm_compisitional_abstractor=create_tfm_compisitional_abstractor,
